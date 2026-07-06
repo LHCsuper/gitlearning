c5d7fbf081cb02afcc02971ca15952b4d8ceaee1
LHC <1291540159@qq.com>
2026-07-06 14:32:44 +0800

    2

---
+++ src/pkg01_helloworld_py/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'pkg01_helloworld_py'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='rx01336@openloong.net',
+    maintainer_email='1291540159@qq.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'helloworld = pkg01_helloworld_py.helloworld:main'
+        ],
+    },
+)
