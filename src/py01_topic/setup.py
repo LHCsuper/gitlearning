b69552c2bcb7886500160169f2c63c9be112d1ed
LHC <1291540159@qq.com>
2026-07-07 09:20:20 +0800

    123

--- src/py01_topic/setup.py
+++ src/py01_topic/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-package_name = 'pkg01_helloworld_py'
+package_name = 'py01_topic'
 
 setup(
     name=package_name,
@@ -24,7 +24,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'helloworld = pkg01_helloworld_py.helloworld:main'
+            'demo01_talker_str_py = py01_topic.demo01_talker_str_py:main'
         ],
     },
 )
